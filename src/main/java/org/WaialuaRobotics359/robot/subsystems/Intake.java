e2ae02d413168b0e8434cca7d0ce173742edfcbc
Bowen <104660210+bowenCassel359@users.noreply.github.com>
2023-05-18 20:05:40 -0700

    add basic logging

--- src/main/java/org/WaialuaRobotics359/robot/subsystems/Intake.java
+++ src/main/java/org/WaialuaRobotics359/robot/subsystems/Intake.java
@@ -7,11 +7,15 @@ import org.WaialuaRobotics359.robot.Robot;
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.TalonFX;
 
+import edu.wpi.first.util.datalog.DataLog;
+import edu.wpi.first.util.datalog.StringLogEntry;
+import edu.wpi.first.wpilibj.DataLogManager;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 
 public class Intake extends SubsystemBase {
     private TalonFX mIntakeMotor;
+    private StringLogEntry myStringLog;
     public Intake() {
         mIntakeMotor = new TalonFX(Constants.Intake.intakeID);
 
@@ -19,10 +23,13 @@ public class Intake extends SubsystemBase {
         mIntakeMotor.configAllSettings(Robot.ctreConfigs.intakeFXConfig);
         mIntakeMotor.setInverted(Constants.Intake.intakeMotorInvert);
         mIntakeMotor.setNeutralMode(Constants.Intake.intakeNeutralMode);
+        DataLog log = DataLogManager.getLog();
+        myStringLog = new StringLogEntry(log, "/my/string");
     }
 
     public void intake(double intakeSpeed) {
         mIntakeMotor.set(ControlMode.PercentOutput, intakeSpeed);
+        myStringLog.append("intakeSpeed: " + intakeSpeed);
     }
 
     public void outake(double intakeSpeed) {
